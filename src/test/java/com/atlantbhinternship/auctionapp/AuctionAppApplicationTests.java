1214eb912f950012dbb23ad6ce8e2599c5fed1ef
AminaSehic <asehic2@etf.unsa.ba>
2021-06-21 15:58:30 +0200

    added project skeleton

---
+++ src/test/java/com/atlantbhinternship/auctionapp/AuctionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlantbhinternship.auctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
